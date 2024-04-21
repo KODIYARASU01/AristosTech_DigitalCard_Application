2cc90f335fe71d7c18dc2ba9bedde2ac9026a5e4
KODIYARASU01 <kodiyarasu02gmail.com>
2024-04-21 20:36:28 +0530

    NewDigitalCard3 change to home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -402,36 +402,36 @@ const App = () => {
       >
         <Suspense fallback={<h4>Loading.....</h4>}>
           <Routes>
-            <Route
+            {/* <Route
               path="/"
               element={
                 user ? <Navigate to={`/admin/${user.id}`} /> : <SignIn />
               }
-            />
-            <Route
+            /> */}
+            {/* <Route
               path="/register"
               element={
                 user ? <Navigate to={`/admin/${user.id}`} /> : <SignUp />
               }
-            />
-            <Route
+            /> */}
+            {/* <Route
               path="/admin/:id"
               element={user ? <AdminPannel /> : <SignIn />}
-            />
+            /> */}
             {/* You can use your authRoutes with useAuthRoutes hook here if needed */}
 
-            <Route path={`/Digital_Card/:id`} element={<NewCard2 />} />
+            {/* <Route path={`/Digital_Card/:id`} element={<NewCard2 />} /> */}
 
-            <Route path="/super_admin" element={<SuperAdminPanel />} />
+            {/* <Route path="/super_admin" element={<SuperAdminPanel />} /> */}
 
-            <Route path="/update_user/:id" element={<Update_User_Form />} />
-            <Route
+            {/* <Route path="/update_user/:id" element={<Update_User_Form />} /> */}
+            {/* <Route
               path="/super_admin_register"
               element={<Super_Admin_Register />}
-            />
+            /> */}
             <Route path="/new_card/:id" element={<NewCardDesign1 />} />
             <Route path="/new_card1/:id" element={<NewCardDesign2 />} />
-            <Route path="/new_card3" element={<NewCardDesign3/>}/>
+            <Route path="/" element={<NewCardDesign3/>}/>
             <Route path="/all_products" element={<AllProducts />} />
             <Route path="/all_services" element={<AllServices />} />
             <Route path="/New_products/:id" element={<AllProducts2 />} />
